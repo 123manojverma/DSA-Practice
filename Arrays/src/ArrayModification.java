dca8a4160180e69a220a9295e94efa4a65ef0797
Manoj <123manojverma2005@gmail.com>
2026-01-19 17:11:58 +0530

    Array Modification

--- Arrays/src/ArrayModification.java
+++ Arrays/src/ArrayModification.java
@@ -1,4 +1,47 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class ArrayModification {
+    public static void main(String[] args) {
+        ArrayList<Integer>arr=new ArrayList<>(Arrays.asList(1,2,3,5,9,4,4,5,5));
+//        Insert in end
+        arr.add(1);
+
+//        Insert at Beginning
+        arr.add(0,3);
+
+//        Insert at any position
+        arr.add(1,5);
+
+//        Delete Element from Start
+        arr.remove(0);
+        arr.removeFirst();
+
+//        Delete Element from last
+        arr.remove(arr.size()-1);
+        arr.removeLast();
+
+//        Delete by Index
+        arr.remove(1);
+
+//        Delete by Value(First Occurrence)
+        arr.remove(Integer.valueOf(9));
+
+//        Delete by Value(All Occurrence)
+        arr.removeIf(num->num==1);
+
+//        Update at index
+        arr.set(4,1000);
+
+//        Update the value
+        for(int i=0;i<arr.size();i++){
+            if(arr.get(i)==1000){
+                arr.set(i,4);
+            }
+        }
+
+        for(int i=0;i<arr.size();i++){
+            System.out.print(arr.get(i)+" ");
+        }
+    }
 }
--- Arrays/src/ProblemSet2.java
+++ Arrays/src/ProblemSet2.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
 
 public class ProblemSet2 {
+
+    public static void leftRotate(int[] arr){
+        int temp=arr[0];
+        for(int i=1;i< arr.length;i++){
+            arr[i-1]=arr[i];
+        }
+        arr[arr.length-1]=temp;
+    }
+
+    public static void rightRotate(int[] arr) {
+        int temp=arr[arr.length-1];
+        for(int i= arr.length-2;i>=0;i--){
+            arr[i+1]=arr[i];
+        }
+        arr[0]=temp;
+    }
+
+    public static void main(String[] args) {
+        int[] arr={1,2,3,5,4,6,5,9};
+//        leftRotate(arr);
+        rightRotate(arr);
+        System.out.println(Arrays.toString(arr));
+    }
 }
